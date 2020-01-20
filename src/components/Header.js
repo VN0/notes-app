336d4acc84c46411aaa5ed3f3f0cdaeb62f30b8d
Cody Bennett <hi@codyb.co>
2020-01-20 15:25:40 -0600

    Moved controls to editor and configured autosave.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,49 +1,20 @@
-import React, { useState } from 'react';
-import styled, { css } from 'styled-components/macro';
-import { NavLink } from 'components/Link';
-import Icon from 'components/Icon';
+import React from 'react';
+import styled from 'styled-components/macro';
+import { Link } from 'components/Link';
 import { ReactComponent as LogoIcon } from 'assets/logo.svg';
 import { rgba } from 'utils/style';
 
-export default function Header(props) {
-  const { location } = props;
-  const [hashKey, setHashKey] = useState();
-
-  const handleNavClick = () => {
-    setHashKey(Math.random().toString(32).substr(2, 8));
-  };
-
-  const isMatch = ({ match, hash = '' }) => {
-    if (!match) return false;
-    return `${match.url}${hash}` === `${location.pathname}${location.hash}`;
-  };
-
-  return (
-    <React.Fragment>
-      <HeaderWrapper>
-        <Logo
-          to={{ pathname: '/', hash: '#home', state: hashKey }}
-          aria-label="Notes App by Cody Bennett"
-          onClick={handleNavClick}
-        >
-          <LogoIcon />
-          Notes App
-        </Logo>
-      </HeaderWrapper>
-      <ButtonWrapper>
-        <Button
-          wide
-          disabled={isMatch({ match: { url: '/' } })}
-        >
-          Share
-        </Button>
-        <Button>
-          <Icon icon="plus" />
-        </Button>
-      </ButtonWrapper>
-    </React.Fragment>
-  );
-}
+const Header = () => (
+  <HeaderWrapper>
+    <Logo
+      to="/"
+      aria-label="Notes App by Cody Bennett"
+    >
+      <LogoIcon />
+      Notes App
+    </Logo>
+  </HeaderWrapper>
+);
 
 const HeaderWrapper = styled.nav`
   display: flex;
@@ -55,7 +26,7 @@ const HeaderWrapper = styled.nav`
   z-index: 999;
 `;
 
-const Logo = styled(NavLink)`
+const Logo = styled(Link)`
   display: grid;
   grid-template-columns: 45px 103px;
   grid-gap: 12px;
@@ -70,51 +41,9 @@ const Logo = styled(NavLink)`
 
   &:hover,
   &:active,
-  &:focus,
-  &.active {
+  &:focus {
     color: ${props => rgba(props.theme.colorAccent, 0.8)};
   }
 `;
 
-const ButtonWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  position: fixed;
-  bottom: 0;
-  padding: 20px;
-  width: 100%;
-  z-index: 999;
-`;
-
-const Button = styled.button`
-  outline: 0;
-  border: none;
-  color: ${props => props.disabled
-    ? rgba(props.theme.colorText, 0.2)
-    : props.theme.colorWhite};
-  border-radius: 30px;
-  height: 52px;
-  width: 52px;
-  transition: opacity 0.3s;
-
-  ${props => !props.disabled && css`
-    background: ${props => props.theme.colorAccent};
-    cursor: pointer;
-  `}
-
-  ${props => props.wide && css`
-    width: 120px;
-    border: 1px solid ${props => props.disabled
-      ? rgba(props.theme.colorText, 0.2)
-      : props.theme.colorAccent};
-    font-size: 14px;
-    letter-spacing: 2px;
-  `}
-
-  &:hover,
-  &:active,
-  &:focus {
-    opacity: 0.8;
-  }
-`;
+export default Header;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,43 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React, { useState, useCallback } from 'react';
+import styled, { css } from 'styled-components/macro';
 import { Helmet } from 'react-helmet-async';
+import Icon from 'components/Icon';
 import Input from 'components/Input';
+import { useFormInput } from 'hooks';
+import { rgba } from 'utils/style';
 
 export default function Home() {
+  const [saving, setSaving] = useState();
+  const title = useFormInput('');
+  const content = useFormInput('');
+
+  useCallback(async () => {
+    if (saving) return;
+
+    try {
+      setSaving(true);
+
+      const response = await fetch('/functions/save', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          title: title.value,
+          content: content.value,
+        }),
+      });
+
+      if (response.status !== 200) throw new Error('An error occured while saving your note.');
+
+      setSaving(false);
+    } catch (error) {
+      setSaving(false);
+      alert(error.message);
+    }
+  }, [title.value, content.value, saving]);
+
   return (
     <React.Fragment>
       <Helmet
@@ -17,11 +51,13 @@ export default function Home() {
         <EditorWrapper>
           <EditorContent>
             <TitleInput
+              {...title}
               autoComplete="off"
               label="Type your title"
               maxLength={320}
             />
-            <ParagraphInput
+            <ContentInput
+              {...content}
               autoComplete="off"
               label="Type or paste your text here."
               maxLength={5000}
@@ -30,6 +66,12 @@ export default function Home() {
           </EditorContent>
         </EditorWrapper>
       </EditorScrollFix>
+      <ButtonWrapper>
+        <Button wide>Share</Button>
+        <Button>
+          <Icon icon="plus" />
+        </Button>
+      </ButtonWrapper>
     </React.Fragment>
   );
 }
@@ -65,7 +107,50 @@ const TitleInput = styled(Input)`
   font-size: 32px;
 `;
 
-const ParagraphInput = styled(Input)`
+const ContentInput = styled(Input)`
   font-size: 16px;
   opacity: 0.8;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: fixed;
+  bottom: 0;
+  padding: 20px;
+  width: 100%;
+  z-index: 999;
+`;
+
+const Button = styled.button`
+  outline: 0;
+  border: none;
+  color: ${props => props.disabled
+    ? rgba(props.theme.colorText, 0.2)
+    : props.theme.colorWhite};
+  border-radius: 30px;
+  height: 52px;
+  width: 52px;
+  transition: opacity 0.3s;
+
+  ${props => !props.disabled && css`
+    background: ${props => props.theme.colorAccent};
+    cursor: pointer;
+  `}
+
+  ${props => props.wide && css`
+    width: 120px;
+    border: 1px solid ${props => props.disabled
+      ? rgba(props.theme.colorText, 0.2)
+      : props.theme.colorAccent};
+    font-size: 14px;
+    letter-spacing: 2px;
+  `}
+
+  &:hover,
+  &:active,
+  &:focus {
+    opacity: 0.8;
+  }
 `;
